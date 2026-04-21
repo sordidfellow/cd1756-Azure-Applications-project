cbea084856dc23cb81e5c43e959a1bbe9f28fee6
Stephen <xjc599yddv@snkmail.com>
2026-04-20 21:28:39 -0500

    more logging

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,9 +1,7 @@
 """
 The flask application package.
 """
-import logging
-import os
-from logging.handlers import RotatingFileHandler
+from logging.config import dictConfig
 
 from flask import Flask
 from config import Config
@@ -11,39 +9,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
 
-
-def setup_logging(app):
-    """Configure logging to stream and file."""
-    if not app.debug:
-        # Create logs directory if it doesn't exist
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        
-        # Create rotating file handler
-        file_handler = RotatingFileHandler('logs/app.log', 
-                                           maxBytes=10240000,  # 10MB
-                                           backupCount=10)
-        file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
-        ))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
-    
-    # Console stream handler
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
-    ))
-    stream_handler.setLevel(logging.DEBUG)
-    app.logger.addHandler(stream_handler)
-    
-    app.logger.setLevel(logging.DEBUG)
-    app.logger.info('Application startup')
-
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s [in %(pathname)s:%(lineno)d]',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 app.config.from_object(Config)
-setup_logging(app)
 
 Session(app)
 db = SQLAlchemy(app)
