d901bb18f2adb66ed0e06ea75d7358afbf3d1370
Stephen <xjc599yddv@snkmail.com>
2026-04-20 22:32:12 -0500

    add lots of logging and forcing of https to views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -33,7 +33,7 @@ def new_post():
     if form.validate_on_submit():
         post = Post()
         post.save_changes(form, request.files['image_path'], current_user.id, new=True)
-        return redirect(url_for('home'))
+        return redirect(url_for('home', _scheme="https"))
     return render_template(
         'post.html',
         title='Create Post',
@@ -49,7 +49,7 @@ def post(id):
     form = PostForm(formdata=request.form, obj=post)
     if form.validate_on_submit():
         post.save_changes(form, request.files['image_path'], current_user.id)
-        return redirect(url_for('home'))
+        return redirect(url_for('home', _scheme="https"))
     return render_template(
         'post.html',
         title='Edit Post',
@@ -59,57 +59,61 @@ def post(id):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    app.logger.info("/login - endpoint starting...")
+    app.logger.debug(f"/login[{request.method}] - endpoint starting...")
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('home', _scheme="https"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             app.logger.warning("/login - user failed to log in")
             flash('Invalid username or password')
-            return redirect(url_for('login'))
-        app.logger.info("/login - user successfully logged in with username/password")
+            return redirect(url_for('login', _scheme="https"))
+        app.logger.info(f"/login - user {user} successfully logged in with a form-provided username/password")
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('home')
+            next_page = url_for('home', _scheme="https")
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
-    app.logger.info("/login - starting auth url flow...")
+    app.logger.debug("/login - starting auth url flow...")
     auth_url = _build_auth_url(authority=Config.AUTHORITY, scopes=Config.SCOPE, state=session["state"])
     return render_template('login.html', title='Sign In', form=form, auth_url=auth_url)
 
 @app.route(Config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
 def authorized():
-    app.logger.info("/authorized - endpoint starting...")
+    app.logger.debug(f"/authorized ({Config.REDIRECT_PATH}) - endpoint starting...")
     if request.args.get('state') != session.get("state"):
-        app.logger.error("/authorized - missing state or mismatch on state")
-        return redirect(url_for("home"))  # No-OP. Goes back to Index page
+        app.logger.error(f"/authorized ({Config.REDIRECT_PATH}) - missing state or mismatch on state")
+        return redirect(url_for("home", _scheme="https"))  # No-OP. Goes back to Index page
     if "error" in request.args:  # Authentication/Authorization failure
-        app.logger.error(f"/authorized - error in request.args: {request.args}")
+        app.logger.error(f"/authorized ({Config.REDIRECT_PATH}) - error in request.args: {request.args}, User failed login attempt.")
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
-        app.logger.info("/authorized - checking status of values....")
+        app.logger.debug(f"/authorized ({Config.REDIRECT_PATH}) - checking status of values....")
         cache = _load_cache()
+        app.logger.debug(f"/authorized ({Config.REDIRECT_PATH}) - token cache loaded....")
         result = _build_msal_app().acquire_token_by_auth_code_flow(
             session.get("flow", {}), request.args)
         if "error" in result:
-            app.logger.info(f"/authorized - found error in result values: {result}")
+            app.logger.error(f"/authorized ({Config.REDIRECT_PATH}) - found error in result values: {result}")
             return render_template("auth_error.html", result=result)
-        app.logger.info("/authorized - extracting claims...")
+        app.logger.debug(f"/authorized ({Config.REDIRECT_PATH}) - extracting claims...")
         session["user"] = result.get("id_token_claims")
         # Note: In a real app, we'd use the 'name' property from session["user"] below
         # Here, we'll use the admin username for anyone who is authenticated by MS
+        app.logger.info(f"/authorized ({Config.REDIRECT_PATH}) - Allowing login for ADMIN account!")
         user = User.query.filter_by(username="admin").first()
+        app.logger.debug(f"/authorized ({Config.REDIRECT_PATH}) - Running login_user ")
         login_user(user)
+        app.logger.debug(f"/authorized ({Config.REDIRECT_PATH}) - Saving token cache")
         _save_cache(cache)
-    app.logger.warning("/authorized - reached end of func")
-    return redirect(url_for('home'))
+    app.logger.debug(f"/authorized ({Config.REDIRECT_PATH}) - reached end of func")
+    return redirect(url_for('home', _scheme="https"))
 
 @app.route('/logout')
 def logout():
-    app.logger.info("/logout - called")
+    app.logger.debug("/logout - called")
     logout_user()
     if session.get("user"): # Used MS Login
         # Wipe out user and its token cache from session
@@ -117,9 +121,9 @@ def logout():
         # Also logout from your tenant's web session
         return redirect(
             Config.AUTHORITY + "/oauth2/v2.0/logout" +
-            "?post_logout_redirect_uri=" + url_for("login", _external=True))
+            "?post_logout_redirect_uri=" + url_for("login", _external=True, _scheme="https"))
 
-    return redirect(url_for('login'))
+    return redirect(url_for('login', _scheme="https"))
 
 def _load_cache():
     cache = msal.SerializableTokenCache()
