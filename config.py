ca7aba3eac80492f6c263045d6f9b2b526246e6e
Stephen <xjc599yddv@snkmail.com>
2026-04-20 23:23:45 -0500

    remove unused env var

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     PREFERRED_URL_SCHEME = "https"
 
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
+    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
     BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
