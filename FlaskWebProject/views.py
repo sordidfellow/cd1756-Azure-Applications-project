bdcdf32017e6bb698aedbf5086a173772f8505f3
Stephen <xjc599yddv@snkmail.com>
2026-04-20 22:38:45 -0500

    fix _scheme, add screenshots

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -33,7 +33,7 @@ def new_post():
     if form.validate_on_submit():
         post = Post()
         post.save_changes(form, request.files['image_path'], current_user.id, new=True)
-        return redirect(url_for('home', _scheme="https"))
+        return redirect(url_for('home'))
     return render_template(
         'post.html',
         title='Create Post',
@@ -49,7 +49,7 @@ def post(id):
     form = PostForm(formdata=request.form, obj=post)
     if form.validate_on_submit():
         post.save_changes(form, request.files['image_path'], current_user.id)
-        return redirect(url_for('home', _scheme="https"))
+        return redirect(url_for('home'))
     return render_template(
         'post.html',
         title='Edit Post',
@@ -61,19 +61,19 @@ def post(id):
 def login():
     app.logger.debug(f"/login[{request.method}] - endpoint starting...")
     if current_user.is_authenticated:
-        return redirect(url_for('home', _scheme="https"))
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             app.logger.warning("/login - user failed to log in")
             flash('Invalid username or password')
-            return redirect(url_for('login', _scheme="https"))
+            return redirect(url_for('login'))
         app.logger.info(f"/login - user {user} successfully logged in with a form-provided username/password")
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('home', _scheme="https")
+            next_page = url_for('home')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     app.logger.debug("/login - starting auth url flow...")
@@ -85,7 +85,7 @@ def authorized():
     app.logger.debug(f"/authorized ({Config.REDIRECT_PATH}) - endpoint starting...")
     if request.args.get('state') != session.get("state"):
         app.logger.error(f"/authorized ({Config.REDIRECT_PATH}) - missing state or mismatch on state")
-        return redirect(url_for("home", _scheme="https"))  # No-OP. Goes back to Index page
+        return redirect(url_for("home"))  # No-OP. Goes back to Index page
     if "error" in request.args:  # Authentication/Authorization failure
         app.logger.error(f"/authorized ({Config.REDIRECT_PATH}) - error in request.args: {request.args}, User failed login attempt.")
         return render_template("auth_error.html", result=request.args)
@@ -109,7 +109,7 @@ def authorized():
         app.logger.debug(f"/authorized ({Config.REDIRECT_PATH}) - Saving token cache")
         _save_cache(cache)
     app.logger.debug(f"/authorized ({Config.REDIRECT_PATH}) - reached end of func")
-    return redirect(url_for('home', _scheme="https"))
+    return redirect(url_for('home'))
 
 @app.route('/logout')
 def logout():
@@ -123,7 +123,7 @@ def logout():
             Config.AUTHORITY + "/oauth2/v2.0/logout" +
             "?post_logout_redirect_uri=" + url_for("login", _external=True, _scheme="https"))
 
-    return redirect(url_for('login', _scheme="https"))
+    return redirect(url_for('login'))
 
 def _load_cache():
     cache = msal.SerializableTokenCache()
